35153548272aaacccec61b4167a004e2cfaa0e96
Brian Romanko <hello@bromanko.com>
2025-03-14 16:05:47 -0700

    Fix up installPhase

--- nix/dealmail.nix
+++ nix/dealmail.nix
@@ -18,10 +18,29 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = lib.fakeHash;
+    hash = "sha256-PkziW+SDNrMuH6gZjim49ax2IXLLeHRyXmIutoRrO7c=";
   };
 
   postBuild = ''
     pnpm run build
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/{bin,lib/node_modules/dealmail}
+    
+    # Copy the built app and dependencies to lib/node_modules
+    cp -r package.json pnpm-lock.yaml dist node_modules $out/lib/node_modules/dealmail/
+    
+    # Create executable wrapper script
+    cat > $out/bin/dealmail << EOF
+    #!/usr/bin/env bash
+    NODE_PATH=$out/lib/node_modules/dealmail/node_modules exec ${nodejs}/bin/node $out/lib/node_modules/dealmail/dist/index.js "\$@"
+    EOF
+    
+    chmod +x $out/bin/dealmail
+
+    runHook postInstall
   '';
 })
